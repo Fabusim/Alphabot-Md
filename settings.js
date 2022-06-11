95b3382f64e50151e4a4592f1928f9ae0f32b926
Fabusim <106881541+Fabusim@users.noreply.github.com>
2022-06-11 11:53:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Kinemon`Bot" //namabot kalian
-global.ownername= "putra" //nama kalian
+global.botname = "Botz_WhatsApp" //namabot kalian
+global.ownername= "Dms_Owner" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "putrabotz@gmail.com" //bebas
+global.email = "botwhatsapp@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "6281286529837" // nomor wa kalian
-global.ownernomerr = "+6281286529837" //nmr wa kalian
+global.ownernomer = "6285817421330" // nomor wa kalian
+global.ownernomerr = "+6285817421330" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6281286529837","6285817421330"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© putra' //sticker wm ubah
-global.author = 'Di Buat Oleh Kinemon`Bot' //sticker wm ganti nama kalian
+global.owner = ["6285817421330"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© Botz_WhatsApp' //sticker wm ubah
+global.author = 'Di Buat Oleh Dms_Owner' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
